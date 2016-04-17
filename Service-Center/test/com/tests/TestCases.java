8cd891cd7ce24b4844a1d474c7cfec962d7a25cc
blessing <Hope.neverfail@gmail.com>
2016-04-17 18:21:32 +0530

    unit test case class is created

---
+++ Service-Center/test/com/tests/TestCases.java
@@ -0,0 +1,35 @@
+package com.tests;
+ 
+import org.junit.Test;
+
+import com.garage.beans.*;
+import com.garage.dao.*;
+
+public class TestCases {
+	
+  private User user;
+  private RepairJobDAO repairJobDAO;	 
+  public TestCases() {
+    user = new User();
+    repairJobDAO = new RepairJobDAO();
+  }	
+  
+  
+ @Test
+ public void testValidUser() {
+     assert user.validateUser("manager", "manager");
+ }
+ @Test
+ public void testInValidUser() {
+     assert !user.validateUser("manager", "wrongpassword");
+ }
+  @Test
+  public void testaddNewPendingRepairJob() {
+      assert repairJobDAO.addNewPendingRepairJob("Ford,", "New Exhust", "FR-3030"); 
+  }
+  @Test
+  public void testUpdateJobStatus () {
+      assert repairJobDAO.updateJobStatus(0,"Completed"); 
+  }
+ 
+}
